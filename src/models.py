12271a60ea157e03f2e1e7b9c90d0c5c1f581c8f
rafael <rafael@lyem.localdomain>
2020-05-09 17:57:12 -0300

    init project

---
+++ src/models.py
@@ -0,0 +1,36 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+engine =  create_engine('sqlite:///contas.db', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+class Empresa(Base):
+    __tablename__='empresa'
+    id = Column(Integer, primary_key=True)
+    email = Column(String, index=True)
+    senha = Column(String)
+    nome = Column(String)
+
+class Clientes(Base):
+    __tablename__='clientes'
+    id = Column(Integer, primary_key=True)
+    email = Column(String, index=True)
+    senha = Column(String)
+    nome = Column(String)
+
+class EmpresaConf(Base):
+    __tablename__='empresaconf'
+    id = Column(Integer, primary_key=True)
+    empresa_id = Column(Integer, ForeignKey('empresa.id'))
+    empresa = relationship('Empresa')
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+if __name__ == '__main__':
+    init_db()
