29942943965a6e8b8a745c2cbe6c289d693a0c08
aheitor27 <62069271+aheitor27@users.noreply.github.com>
2020-06-15 17:54:48 -0300

    adicionei tabela datacliente e dataempresa

--- src/models.py
+++ src/models.py
@@ -12,5 +12,7 @@ def init():
     cursor.execute('CREATE TABLE empresa(id INT AUTO_INCREMENT PRIMARY KEY,usuario VARCHAR(255) NOT NULL UNIQUE,senha VARCHAR(255) NOT NULL,nome VARCHAR(255) NOT NULL UNIQUE,numero VARCHAR(255) NOT NULL,endereco VARCHAR(255) NOT NULL,bairro VARCHAR(255) NOT NULL,telefone VARCHAR(255) NOT NULL,cidade VARCHAR(255) NOT NULL,uf VARCHAR(2) NOT NULL,dinheiro VARCHAR(3) NOT NULL,credito VARCHAR(3) NOT NULL,debito VARCHAR(3) NOT NULL,INDEX (nome),INDEX (usuario))')
     cursor.execute('CREATE TABLE clientes(id INT AUTO_INCREMENT PRIMARY KEY,usuario VARCHAR(255) NOT NULL UNIQUE,senha VARCHAR(255) NOT NULL,nome VARCHAR(255) NOT NULL UNIQUE,numero VARCHAR(255) NOT NULL,endereco VARCHAR(255) NOT NULL,bairro VARCHAR(255) NOT NULL,telefone VARCHAR(255) NOT NULL,cidade VARCHAR(255) NOT NULL,uf VARCHAR(2) NOT NULL,INDEX (nome),INDEX (usuario))')
     cursor.execute('CREATE TABLE servicos(id INT AUTO_INCREMENT PRIMARY KEY,nome VARCHAR(255) NOT NULL UNIQUE,preco REAL NOT NULL,horario TIME NOT NULL,FOREIGN KEY(id) REFERENCES empresa(id))')
+    cursor.execute('CREATE TABLE datacliente(id INT AUTO_INCREMENT PRIMARY KEY,FOREIGN KEY(id) REFERENCES clientes(id),FOREIGN KEY(nome) REFERENCES servicos(nome), data DATE NOT NULL, horarioinicio TIME NOT NULL)')
+    cursor.execute('CREATE TABLE dataempresa(id INT AUTO_INCREMENT PRIMARY KEY,FOREIGN KEY(id) REFERENCES empresa(id),FOREIGN KEY(nome) REFERENCES clientes(nome), data DATE NOT NULL, horarioinicio TIME NOT NULL, horariofim TIME NOT NULL)')
 if __name__ == '__main__':
     init()
